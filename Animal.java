de4aa3a61d329de95053c1a737af275c03a52e29
Art <art-75@yandex.ru>
2023-03-09 22:39:41 +0300

    Animal

---
+++ Animal.java
@@ -0,0 +1,20 @@
+public abstract class Animal {
+    private int height;
+    private int weight;
+    private String еyeColor;
+
+    protected Animal(int height, int weight, String еyeColor) {
+        this.height = height;
+        this.weight = weight;
+        this.еyeColor = еyeColor;
+    }
+
+    public abstract void animalSay();
+
+    @Override
+    public String toString() {
+        return String.format("Рост: %d; Вес: %d; Цвет глаз: %s", this.height, this.weight, this.еyeColor);
+    }
+
+    
+}
